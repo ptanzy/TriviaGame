203355bb4fca090a5b3f346edc50db2b8f887854
ptanzy <ptanzy@comcast.net>
2018-12-13 23:32:40 -0500

    started writing logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ function Question(question, answer) {
   this.answer = answer;
 }
 
-var question = [
+var questions = [
   new Question("Inspired the French to enter the American Revolution.", "SARATOGA"),
   new Question("Initiated the War between the Confederacy and the Union.", "FORT SUMPTER"),
   new Question("Inspired the French to enter the American Revolution.", "SARATOGA"),
@@ -27,7 +27,57 @@ var question = [
 
 var answers = ["SARATOGA", "FORT SUMPTER", "BUNKER HILL", "GETTYSBURG", "YORKTOWN", "BULL RUN", "LEXINGTON AND CONCORD", "ANTIETAM", 
                "TRENTON","HASTINGS", "STALINGRAD", "VIENNA", "WATERLOO", "MIDWAY", "TRAFALGAR", "AGINCOURT", "VERDUN", "BULGE"];
+var result = {
+  0: "Incorrect",
+  1: "Correct!"
+};
+var time = 15;
 
+function randomize(arr) {
+  var curIdx = arr.length, tempVal, rndIdx;
+
+  // While there remain elements to shuffle...
+  while (0 !== curIdx) {
+
+    // Pick a random index between 0 and current
+    rndIdx = Math.floor(Math.random() * curIdx);
+    //Want our current to index to decrement
+    curIdx -= 1;
+
+    //Switch this element with the current one
+    //Starting from the end of the array we randomize
+    //it by replacing an element with a random one before it
+    tempVal = arr[curIdx];
+    arr[curIdx] = arr[rndIdx];
+    arr[rndIdx] = tempVal;
+  }
+
+  return arr;
+}
+
+//recursively iterates through all questions, asks each one followed by results
+function showQuestion(num) {
+  //if the number is out of bounds or results in only 1 answer
+  if(num >= questions.length || num <= 1) {
+    num = questions.length-1; //sets it to the greatest index
+  }
+  if(num >= 0) {
+    question = question[num];
+    answers = randomize(answers);
+
+    return showQuestion(--num);
+  } else {
+    //TODO show correct/incorrect if incorrect also show question and correct answer
+    return;
+  }
+
+}
+
+function setup() {
+  questions = randomize(questions);
+  //answers = randomize(answers);
+}
+          
 $(window).on('load', function() {
   setup();
 });
